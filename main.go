9265532bc4c09546a60dc392af1800249b9d4c47
wuyujie <wuyujie@qianxin.com>
2020-04-08 16:20:57 +0800

    最简单的限速工具

--- main.go
+++ main.go
@@ -47,43 +47,6 @@ func init() {
 	httpClient = createHTTPClient()
 }
 
-const (
-	DEFAULT_TIME_DURATION = 1000 //s
-)
-
-type LeakBucket struct {
-	Tokens   int64
-	queue    chan int64
-	internal time.Duration
-}
-
-func CreateRateLimit(tokens int64, duration time.Duration) *LeakBucket {
-	l := &LeakBucket{
-		Tokens:   tokens,
-		queue:    make(chan int64, tokens),
-		internal: time.Duration(duration),
-	}
-	go l.Generate()
-	return l
-}
-
-func (s *LeakBucket) Generate() {
-
-	inter := time.NewTicker(time.Millisecond * s.internal)
-	for _ = range inter.C {
-		s.queue <- 1
-	}
-}
-
-func (s *LeakBucket) Take() bool {
-	<-s.queue
-	return true
-}
-
-func (s *LeakBucket) TakeT() time.Time {
-	<-s.queue
-	return time.Now()
-}
 
 func main() {
 
